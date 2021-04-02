8047e5a11ce5ef708eb2e7ea937190cf9141867e
DangToan <dtoan2805@gmail.com>
2021-04-02 08:10:51 -0700

    add checkout page

---
+++ src/user-template/js/checkout.js
@@ -0,0 +1,13 @@
+// let dropdown2 = new UIDropDown(
+//   (idx) => console.log(idx),
+//   "div[local-province]"
+// );
+// let dropdow2n = new UIDropDown(
+//   (idx) => console.log(idx),
+//   "div[local-district]"
+// );
+
+let dropdow22n = new UIDropDown(
+  (idx) => console.log(idx),
+  "div[local-province]"
+);
