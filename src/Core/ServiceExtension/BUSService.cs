c156f51bfdcaa1dfec094ad0b36f8bc8f85cdd12
DangToan <dtoan2805@gmail.com>
2021-04-05 09:09:18 -0700

    Add BUS Dependency service

---
+++ src/Core/ServiceExtension/BUSService.cs
@@ -0,0 +1,19 @@
+using BUS.Interfaces.Services;
+using BUS.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.ServiceExtension
+{
+    public static class BUSService
+    {
+        public static IServiceCollection AddBUS(this IServiceCollection services)
+        {
+
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<ITypeProductService, TypeProductService>();
+            services.AddScoped<IFeeService, FeeService>();
+
+            return services;
+        }
+    }
+}
