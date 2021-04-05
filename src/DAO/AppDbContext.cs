d703ab1f98be3bfbb52577e9fc03aab767ca758f
toan <dtoan2805@gmail.com>
2021-04-05 11:00:13 +0700

    Add DbContext adn DbSet

--- src/DAO/AppDbContext.cs
+++ src/DAO/AppDbContext.cs
@@ -1,10 +1,29 @@
 using System;
+using BUS.Domains;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DAO
 {
-    public class AppDbContext : IdentityDbContext
+    public class AppDbContext : IdentityDbContext<User>
     {
+        public DbSet<TypeProduct> TypeProducts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Fee> Fees { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductDetail> ProductDetails { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+
     }
 }
