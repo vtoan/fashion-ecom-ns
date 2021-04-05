525999f515dbec1634952b1a60f32f15c0c81a3d
DangToan <dtoan2805@gmail.com>
2021-04-05 08:04:05 -0700

    Add Interface Base DAO

--- src/BUS/Interfaces/DAOs/IBaseDAO.cs
+++ src/BUS/Interfaces/DAOs/IBaseDAO.cs
@@ -1,7 +1,17 @@
+using System.Collections.Generic;
+
 namespace BUS.Interfaces.DAOs
 {
-    public interface IBaseDAO
-    {
-         
-    }
+    public interface IGetItem<V> { V Get(int id); }
+    public interface IGetListItems<V> { ICollection<V> GetList(); }
+    public interface IAddItem<V> { V Add(V newObject); }
+    public interface IUpdateItem<V> { bool Update(int id, Dictionary<string, object> newObject); }
+    public interface IDeleteItem<V> { bool Delete(int id); }
+    public interface IBaseDAO<V> :
+        IGetItem<V>,
+        IGetListItems<V>,
+        IAddItem<V>,
+        IUpdateItem<V>,
+        IDeleteItem<V>
+    { }
 }
