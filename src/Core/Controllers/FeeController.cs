c13a34c028d026017e26dbd1d6970e6aa263ca78
DangToan <dtoan2805@gmail.com>
2021-04-07 16:12:40 -0700

    Add Fee api

--- src/Core/Controllers/FeeController.cs
+++ src/Core/Controllers/FeeController.cs
@@ -9,18 +9,51 @@ namespace Core.Controllers
     [Route("[controller]")]
     public class FeeController : ControllerBase
     {
-        private ICategoryService _cateSer;
+        private IFeeService _feeSer;
 
-        public FeeController(ICategoryService cateService)
+        public FeeController(IFeeService feeSer)
         {
-            _cateSer = cateService;
+            _feeSer = feeSer;
         }
 
-
         [HttpGet]
-        public IEnumerable<CategoryVM> GetFees()
+        public IEnumerable<FeeVM> GetList()
         {
-            return _cateSer.GetList();
+            return _feeSer.GetList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<FeeVM> Get(int id)
+        {
+            if (id <= 0) return BadRequest();
+            var result = _feeSer.Get(id);
+            if (result == null) return NotFound();
+            return result;
+        }
+
+        [HttpPost]
+        public ActionResult<FeeVM> Create(FeeVM feeVM)
+        {
+            var result = _feeSer.Add(feeVM);
+            if (result == null) return BadRequest();
+            return CreatedAtAction(nameof(Get), new { id = result.Id }, result);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, FeeVM feeVM)
+        {
+            if (id != feeVM.Id) return BadRequest();
+            var result = _feeSer.Update(id, feeVM);
+            if (!result) return NotFound();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var result = _feeSer.Delete(id);
+            if (!result) return NotFound();
+            return NoContent();
         }
     }
 }
