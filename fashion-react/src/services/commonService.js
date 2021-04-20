6e1cd39342aaa024c70ffb23f7d52af77f929c89
DangToan <dtoan2805@gmail.com>
2021-04-19 17:24:00 -0700

    Fix  create param

--- fashion-react/src/services/commonService.js
+++ fashion-react/src/services/commonService.js
@@ -51,11 +51,12 @@ export default class commonService {
 
   _createQuery(params) {
     if (!params) return "";
-    let queryStr = "?";
+    let queryStr = "";
     for (const key in params) {
       if (!params[key]) continue;
+      if (queryStr) queryStr += "&&";
       queryStr += key + "=" + params[key];
     }
-    return queryStr;
+    return "?" + queryStr;
   }
 }
