306f0d3e8a3dc9384550d6dd05fcda588582ec22
DangToan <dtoan2805@gmail.com>
2021-04-22 00:25:44 -0700

    Add Attribute Product Feature

--- fashion-react/src/services/productService.js
+++ fashion-react/src/services/productService.js
@@ -1,7 +1,78 @@
-// import axios from "axios";
-// import { host } from "../config";
+import axios from "axios";
+import { host } from "../config";
 import commonService from "./commonService";
 
-class productService extends commonService {}
+class productService extends commonService {
+  //attributes
+  createAttr(productId, newAttr) {
+    return axios({
+      url: host + "/" + this.pathSer + "/" + productId + "/attrs",
+      method: "post",
+      data: { Size: newAttr },
+      actionName: `Create new Product Attribute`,
+    });
+  }
+  getListAttr(productId) {
+    return axios({
+      url: host + "/" + this.pathSer + "/" + productId + "/attrs",
+      method: "get",
+      actionName: `Get list Product Attribute of ${productId}`,
+    });
+  }
+
+  deleteAttr(productId, itemId) {
+    return axios({
+      url: host + "/" + this.pathSer + "/" + productId + "/attrs/" + itemId,
+      method: "delete",
+      actionName: `Delete Product Attribute ${itemId}`,
+    });
+  }
+  //image
+  changeImageDefault(productId, newImage) {
+    return axios({
+      url: host + "/" + this.pathSer + "/" + productId,
+      method: "put",
+      data: { Id: productId, Image: newImage },
+      actionName: `Change Image for ${productId}`,
+    });
+  }
+
+  getListImages(productId) {
+    return axios({
+      url: host + "/" + this.pathSer + "/" + productId + "/images",
+      method: "get",
+      actionName: `Get List Image Product`,
+    });
+  }
+
+  uploadImage(productId, newImage) {
+    let formData = new FormData();
+    formData.append("image", newImage);
+    return axios({
+      url: host + "/" + this.pathSer + "/" + productId + "/images",
+      method: "post",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      data: formData,
+      actionName: `Upload Image for ${productId}`,
+    });
+  }
+
+  removeImage(productId, imageName) {
+    return axios({
+      url:
+        host +
+        "/" +
+        this.pathSer +
+        "/" +
+        productId +
+        "/images?name=" +
+        imageName,
+      method: "delete",
+      actionName: `Delete Product Attribute ${productId}`,
+    });
+  }
+}
 
 export default new productService("product", "Product");
